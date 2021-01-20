033c8b74560ddaf38022ffbb1dd67e8c1cb05ac1
Kostya <rodina9180gaocheng@ukr.net>
2021-01-20 20:19:21 +0300

    added gamburger menu

---
+++ app.js
@@ -0,0 +1,15 @@
+// mobile menu
+let mobileMenu = document.querySelector(".nav-mobile-menu");// создаем пер. для меню мобилки.
+let mainMenu = document.querySelector(".menu");// создаем пер. для меню.
+
+console.log(mobileMenu);
+console.log(mainMenu);
+
+ mobileMenu.addEventListener("click",function() {
+    mobileMenu.classList.toggle("active-menu");
+    if(mobileMenu.classList.contains("active-menu")) {
+        mainMenu.classList.add("active-menu");
+    } else {
+        mainMenu.classList.remove("active-menu");
+    }
+})
