9b974af5e8d4f92bf1adad2596a530fb58dea97b
Kostya <rodina9180gaocheng@ukr.net>
2021-02-23 11:48:17 +0300

    added modal

--- app.js
+++ app.js
@@ -2,14 +2,18 @@
 let mobileMenu = document.querySelector(".nav-mobile-menu");// создаем пер. для меню мобилки.
 let mainMenu = document.querySelector(".menu");// создаем пер. для меню.
 
-console.log(mobileMenu);
-console.log(mainMenu);
+// console.log(mobileMenu);
+// console.log(mainMenu);
 
  mobileMenu.addEventListener("click",function() {
-    mobileMenu.classList.toggle("active-menu");
+     mobileMenu.classList.toggle("active-menu");
     if(mobileMenu.classList.contains("active-menu")) {
         mainMenu.classList.add("active-menu");
     } else {
         mainMenu.classList.remove("active-menu");
     }
 })
+
+//modal
+
+    
