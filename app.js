1e2f9ea7ba5ebdde518ab924045e66bb82908e66
Kostya <rodina9180gaocheng@ukr.net>
2021-02-24 15:10:35 +0300

    added modal

--- app.js
+++ app.js
@@ -10,10 +10,50 @@ let mainMenu = document.querySelector(".menu");// создаем пер. для 
     if(mobileMenu.classList.contains("active-menu")) {
         mainMenu.classList.add("active-menu");
     } else {
-        mainMenu.classList.remove("active-menu");
+        mainMenu.classList.remove("active-menu"); 
     }
 })
 
 //modal
+let modal = document.querySelector(".modal");
+let sendMessageBtn = document.querySelector(".more-details");
+let closeBtn = document.querySelector(".btn-close");
+
+console.log(modal)
+console.log(sendMessageBtn)
+console.log(closeBtn)
+
+sendMessageBtn.addEventListener("click",openModal)
+
+function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+}
+
+function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+}
+
+closeBtn.addEventListener("click",closeModal)
+
+modal.addEventListener("click",function(e) {
+    if(e.target == modal) {
+        closeModal()
+    }
+})
+
+//modal scroll
+// console.log(window.pageYOffset)// сколько проскролили
+// console.log(document.body.scrollHeight)// высота странички
+
+function showModalByScroll() {
+    if(window.pageYOffset > document.body.scrollHeight/2) {
+        openModal()
+        window.removeEventListener("scroll",showModalByScroll)// на событии scroll удалить ф. showModalByScroll.
+    }
+}
+
+window.addEventListener("scroll",showModalByScroll);
 
     
